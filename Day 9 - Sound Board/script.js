f78285c2b4f1d50fe1f304aa23feefec6594a7f4
Victor Hugo <victor.devfrontend@gmail.com>
2023-01-14 20:19:11 -0300

    day 9

--- Day 9 - Sound Board/script.js
+++ Day 9 - Sound Board/script.js
@@ -7,8 +7,19 @@ sounds.forEach((sound) => {
   btn.innerText = sound;
 
   btn.addEventListener("click", () => {
+    stopSongs();
+
     document.getElementById(sound).play();
   });
 
   document.getElementById("buttons").appendChild(btn);
 });
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
