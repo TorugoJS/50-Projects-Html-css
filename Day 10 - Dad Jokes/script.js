0ad94ced627c242421ec466ead9812e409575980
Victor Hugo <victor.devfrontend@gmail.com>
2023-01-19 07:43:56 -0300

    day 10

--- Day 10 - Dad Jokes/script.js
+++ Day 10 - Dad Jokes/script.js
@@ -1,12 +1,34 @@
 const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
 
+jokeBtn.addEventListener("click", generateJoke);
+
 generateJoke();
 
-function generateJoke() {
-  fetch("https://icanhazdadjoke.com", {
+async function generateJoke() {
+  const config = {
     headers: {
       Accept: "application/json",
     },
-  });
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
 }
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch("https://icanhazdadjoke.com", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
