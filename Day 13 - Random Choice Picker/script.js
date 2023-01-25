8176e94568213ff07691f6f90aa116c56645d772
Victor <victor.devfrontend@gmail.com>
2023-01-25 11:08:30 -0300

    day 13

--- Day 13 - Random Choice Picker/script.js
+++ Day 13 - Random Choice Picker/script.js
@@ -1,10 +1,18 @@
-const tagsEL = document.getElementById("tags");
+const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
 
 textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key == "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -13,10 +21,49 @@ function createTags(input) {
     .filter((tag) => tag.trim() !== "")
     .map((tag) => tag.trim());
 
-  tags.innerHTML = "";
+  tagsEl.innerHTML = "";
 
   tags.forEach((tag) => {
     const tagEl = document.createElement("span");
     tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.random() * tags.length];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
