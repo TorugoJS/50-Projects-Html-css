ecf1c547068aa24bd47ba465ecee1081fcd981d4
Victor <victor.devfrontend@gmail.com>
2023-01-28 20:03:33 -0300

    day 16

--- Day 16 - Drink Water/script.js
+++ Day 16 - Drink Water/script.js
@@ -3,6 +3,8 @@ const listers = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
+updateBigCup();
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => {
     highlightCups(idx);
@@ -10,7 +12,8 @@ smallCups.forEach((cup, idx) => {
 });
 
 function highlightCups(idx) {
-  if (
+  if (idx === 7 && smallCups[idx].classList.contains("full")) idx--;
+  else if (
     smallCups[idx].classList.contains("full") &&
     !smallCups[idx].nextElementSibling.classList.contains("full")
   ) {
@@ -24,4 +27,28 @@ function highlightCups(idx) {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
