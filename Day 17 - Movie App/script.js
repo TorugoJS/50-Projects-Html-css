4a68eee9eb03e5373b15b444d4ac7066e6a3c1d1
Victor <victor.devfrontend@gmail.com>
2023-01-29 15:33:41 -0300

    day 17

--- Day 17 - Movie App/script.js
+++ Day 17 - Movie App/script.js
@@ -8,8 +8,7 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-// get initial movies
-
+// Get initial movies
 getMovies(API_URL);
 
 async function getMovies(url) {
@@ -29,17 +28,16 @@ function showMovies(movies) {
     movieEl.classList.add("movie");
 
     movieEl.innerHTML = `
-   
-      <img src="${IMG_PATH + poster_path}" alt="${title}">
-      <div class="movie-info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-      </div>
-      <div class="overview">
-        <h3>Overview</h3>
-        ${overview}
-      </div>
-    `;
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `;
     main.appendChild(movieEl);
   });
 }
@@ -47,12 +45,11 @@ function showMovies(movies) {
 function getClassByRate(vote) {
   if (vote >= 8) {
     return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
   }
-}
-if (vote >= 5) {
-  return "orange";
-} else {
-  return "red";
 }
 
 form.addEventListener("submit", (e) => {
