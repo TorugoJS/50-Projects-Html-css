867b485744e56505336520d5aad8bea7fb69f68c
Victor <victor.devfrontend@gmail.com>
2023-02-01 17:26:10 -0300

    day 20

--- Day 20 - Button Ripple Effect/script.js
+++ Day 20 - Button Ripple Effect/script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll(".ripple");
 
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
+  button.addEventListener("click", function (e) {
     const x = e.clientX;
     const y = e.clientY;
 
@@ -10,5 +10,14 @@ buttons.forEach((button) => {
 
     const xInside = x - buttonTop;
     const yInside = y - buttonLeft;
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
   });
 });
