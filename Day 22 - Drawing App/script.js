df804bac2752135e92e8a289efdc1fd604d654ce
Victor <victor.devfrontend@gmail.com>
2023-02-08 09:43:40 -0300

    day 22

---
+++ Day 22 - Drawing App/script.js
@@ -0,0 +1,51 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let size = 20;
+let isPressed = false;
+let color = "black";
+let x;
+let y;
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x2, y2);
+  }
+});
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
+}
+
+// drawCircle(100, 200);
+// drawLine(300, 300, 200, 500);
