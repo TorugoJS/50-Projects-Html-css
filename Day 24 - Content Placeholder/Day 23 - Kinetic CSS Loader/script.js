4cade67fd361521f11c261b60500de5a2da061ca
Victor <victor.devfrontend@gmail.com>
2023-02-11 16:37:56 -0300

    day 24

---
+++ Day 24 - Content Placeholder/Day 23 - Kinetic CSS Loader/script.js
@@ -0,0 +1,6 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
